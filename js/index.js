9db9d06f45cc46c18f39ec6767fe22298fbeef66
Inbal Kimhi <inbalkm100@gmail.com>
2022-11-14 16:53:04 +0200

    end of part one

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function infoButton(){
+    alert('Inbal Kimhi\n stage one\n this app is a calculator app. A calculator is a device that performs arithmetic operations on numbers.\n enjoy your calculations :)');
+}
+
+function displayButtonInfo(value){
+    alert(`${value}`)
+}
