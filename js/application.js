2b29fe317233b2f35c8dbcd28d809138b4ffd80b
Inbal Kimhi <inbalkm100@gmail.com>
2022-11-17 12:02:15 +0200

    numbers displayed on screen

--- js/application.js
+++ js/application.js
@@ -2,6 +2,3 @@ function infoButton(){
     alert('Inbal Kimhi\n stage one\n this app is a calculator app. A calculator is a device that performs arithmetic operations on numbers.\n enjoy your calculations :)');
 }
 
-function displayButtonInfo(value){
-    alert(`${value}`)
-}
--- js/calculation.js
+++ js/calculation.js
@@ -16,17 +16,19 @@ ops.map( x => {
             num1= ''
             op = undefined
             num2 = ''
-            alert(result)
+            document.getElementById('screen').innerText = `${result}`
             
 
         }else if(num1 && num2 && op !== '='){
             num1 = eval(num1 + op + num2);
             op = event.target.innerText;
             num2 = '';
+            document.getElementById('screen').innerText = `${num1} ${op}`
             
 
         }else{
             op = event.target.innerText;
+            document.getElementById('screen').innerText = `${num1} ${op}`
             
         }
         
@@ -37,12 +39,13 @@ const Numbers = Array.from(document.getElementsByClassName("number-buttons"))
 
 Numbers.map( (y) => {
     y.addEventListener('click',(event)=>{
-        if(op=== undefined){
+        if(op === undefined){
             num1 += event.target.innerText;
+            document.getElementById('screen').innerText = `${num1}`
             
         }else{
             num2 += event.target.innerText;
-            
+            document.getElementById('screen').innerText = `${num1} ${op} ${num2}`
         }
     }) 
 })
@@ -51,6 +54,7 @@ document.getElementById('C-button').addEventListener('click',(event) =>{
     num1 = '';
     op = undefined;
     num2 = '';
+    document.getElementById('screen').innerText = ''
     
 }
 
@@ -59,11 +63,14 @@ document.getElementById('C-button').addEventListener('click',(event) =>{
 document.getElementById('erase').addEventListener('click',(event) =>{
     if(num2){
         num2 = num2.replace( num2[num2.length - 1],'')
+        document.getElementById('screen').innerText = `${num1} ${op} ${num2}`
     }
     else if (op !== undefined ){
         op = undefined
+        document.getElementById('screen').innerText = `${num1}`
     }else{
        num1 = num1.replace( num1[num1.length - 1],'')
+       document.getElementById('screen').innerText = `${num1}`
     }
 }
 
